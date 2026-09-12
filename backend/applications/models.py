6ac2efc9938190b07335be646125ea813fce51d7
AnkitaVishw <ankushrma1999@gmail.com>
2026-09-12 20:41:55 +0530

    Add application status managment

--- backend/applications/models.py
+++ backend/applications/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class JobApplication(models.Model):
     STATUS_CHOICES = [
         ("Applied", "Applied"),
@@ -18,7 +19,10 @@ class JobApplication(models.Model):
         choices=STATUS_CHOICES,
         default="Applied",
     )
-
+    salary = models.CharField(max_length=100, blank=True)
+    job_url = models.URLField(blank=True)
+    date_applied = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.company} - {self.role}"
